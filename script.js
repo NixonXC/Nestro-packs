e261e1bfe7fc18305bfa752926a96e423f0db066
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-10-05 09:47:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.getElementById("down").onclick = function ()
 {
     window.location="https://www.mediafire.com/file/zfb1yblkv5crlzx/The_Cursed_Pack.zip/file"
 };
+$(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+      $('#circularcursor').css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    })
+  });
